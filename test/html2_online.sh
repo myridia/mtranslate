9bca92b897b04d9a82c936ae8dab71bda546b9e5
veto <veto@lab.local>
2026-03-21 15:29:00 +0700

    up: src/html_translate.rs test/body.html test/html2.sh test/html2_online.sh

--- test/html2_online.sh
+++ test/html2_online.sh
@@ -3,7 +3,7 @@ echo "start test2"
 html_content=$(cat body.html)
 curl -X POST 'https://mtranslate.myridia.com/' \
      -H 'Content-Type: application/json' \
-     -d "$(jq -n --arg html "$html_content" '{"html":$html,"t":"en","s":"sv"}' )"
+     -d "$(jq -n --arg html "$html_content" '{"html":$html,"t":"de","s":"sv"}' )"
 
 
 
