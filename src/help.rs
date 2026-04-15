4098302100d6004772c5be9c31f94f515636c6c7
veto <veto@myridia.com>
2026-04-15 07:34:01 +0700

    update

--- src/help.rs
+++ src/help.rs
@@ -1,4 +1,4 @@
-use axum::{Json, response::IntoResponse};
+use axum::{response::IntoResponse, Json};
 
 pub async fn help() -> impl IntoResponse {
     // http://127.0.0.1:8889/help
@@ -10,6 +10,31 @@ pub async fn help() -> impl IntoResponse {
             "t": env!("codes"),
             "example" : "https://translate.myridia.com?s=en&t=de&v=hello",
             "hostname": h
+        }
+    ]);
+    Json(r)
+}
+
+pub async fn ftl() -> impl IntoResponse {
+    // http://127.0.0.1:8889/help
+    let h: String = hostname::get().unwrap().into_string().unwrap();
+    let r = serde_json::json!([
+        {
+            "api": "ftl",
+            "ftl": env!("ftl"),
+        }
+    ]);
+    Json(r)
+}
+
+pub async fn codes() -> impl IntoResponse {
+    // http://127.0.0.1:8889/help
+    let h: String = hostname::get().unwrap().into_string().unwrap();
+    let r = serde_json::json!([
+        {
+            "api": "codes",
+            "codes": env!("codes"),
+
         }
     ]);
     Json(r)
